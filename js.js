ce2ad8b0ce02983b8f091326e39c025896a2422a
jwdeitch <jwdeitch@gmail.com>
2016-01-30 11:04:05 -0500

    keyboard interactive results

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    var responseObj = [];
     var googleUrl = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyB20e2VDjrUebicIJkA4MFH4WO4b8cEzQY&cx=013676722247143124300:dazj-lelyfy&num=3&q=';
 
     $('html').click(function () {
@@ -8,7 +8,17 @@ $(document).ready(function () {
 
     $('body').on("keydown", '#queryInputTb', function (e) {
         if (e.which == 13 && $(this).val() !== "") {
-            switch ($(this).val()) {
+            var Qvalue = $(this).val();
+            if (parseInt(Qvalue) && responseObj[Qvalue]) {
+                var win = window.open(responseObj.Qvalue, 'result');
+                win.focus();
+
+                addResult(Qvalue);
+                clearField();
+            }
+
+
+            switch (Qvalue) {
                 case 'c':
                 case 'clear':
                     var me = $(this).parent().parent();
@@ -28,16 +38,18 @@ $(document).ready(function () {
         }
     });
 
-
     function callGoogle(q) {
         $.get(googleUrl + q.val(), function () {
 
             })
             .done(function (data) {
                 if (typeof data['items'] !== 'undefined') {
-                    data['items'].forEach(function (item) {
+                    data['items'].forEach(function (item, index) {
+                        index = index + 1;
+                        responseObj.push(item.link);
+
                         $('.gsh').append('' +
-                            '<div class="result"><div class="title"><a href="'+ item.link +'">' + item.title + '</a></div><div class="snippet">' + item.snippet + '</div><div class="link">' + item.link + '</div></div><hr>');
+                            '<div class="result"><div class="index">'+index+'</div><div class="main"><div class="title"><a href="'+ item.link +'">' + item.title + '</a></div><div class="snippet">' + item.snippet + '</div><div class="link">' + item.link + '</div></div></div><hr>');
                     });
                 } else {
                     addResult('No results');
